729c64667e5ccf8159546decc37cda273824f5ea
Pinhasov_Margarita <45937245+rita10514@users.noreply.github.com>
2025-08-06 05:14:55 +0300

    v1

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5lKrcjasllB":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  window.location.reload();
+
+}
+
